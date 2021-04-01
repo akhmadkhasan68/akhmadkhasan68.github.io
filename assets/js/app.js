db69a802b86c4b7b11535dfac1d31092a6b20d0c
hasan <akhmadkhasan68@gmail.com>
2021-04-01 12:50:48 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var navList = document.getElementById("nav-lists");
+function Show() {
+	navList.classList.add("_Menus-show");
+}
+
+function Hide(){
+	navList.classList.remove("_Menus-show");
+}
