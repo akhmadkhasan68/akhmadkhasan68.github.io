8ccd7a2ab191751cf300481dd519daa44cf05dbe
Hasan <akhmadkhasan68@gmail.com>
2021-04-12 14:20:44 +0700

    add styling dark mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,26 @@ function Hide(){
 
 let year = document.getElementById('year');
 year.innerHTML = new Date().getFullYear();
+
+var element = document.body;
+var icon = document.getElementById('btn-mode');
+
+function switchMode() {
+  element.classList.toggle("dark-mode");
+  icon.classList.toggle("fa-sun");
+
+  if(element.classList.contains('dark-mode')){ //when the body has the class 'dark' currently
+      localStorage.setItem('darkMode', 'enabled'); //store this data if dark mode is on
+  }else{
+      localStorage.setItem('darkMode', 'disabled'); //store this data if dark mode is off
+  }
+}
+
+if (localStorage.getItem('darkMode') === 'enabled') {
+  element.classList.add('dark-mode');
+  icon.classList.add("fa-sun");
+} 
+else{
+  element.classList.remove('dark-mode');
+  icon.classList.add("fa-moon");
+}
